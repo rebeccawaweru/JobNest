ef2d7df38c223caa29f81127fc7ac3eb8e010f9a
uelkariuki <cratzuel@gmail.com>
2024-02-21 02:11:59 +0300

    Secure Job routes from unauthorized users

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,8 +1,12 @@
 import express from 'express';
+import requireAuth from '../middleware/requireAuth.js';
 
 // controller functions
 import { loginUser, registerUser } from '../controllers/userController.js';
 const router = express.Router()
+
+// requires authentication for all job routes
+router.use(requireAuth)
 
 // login route
 router.post('/login', loginUser)
